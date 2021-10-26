4b2e7b78e3ba316f1459e47ef824c716c0dee010
Toricane <73972068+Toricane@users.noreply.github.com>
2021-10-25 18:21:43 -0700

    Add main file

---
+++ bot.py
@@ -0,0 +1,26 @@
+from dis_snek.client import Snake
+from dis_snek.models.enums import Intents
+from dis_snek.models.listener import listen
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Snake(intents=Intents.DEFAULT)
+# intents are what events we want to receive from discord, `DEFAULT` is usually fine
+
+
+@listen()  # this decorator tells snek that it needs to listen for the corresponding event, and run this coroutine
+async def on_ready():
+    # This event is called when the bot is ready to respond to commands
+    print("Ready")
+    print(f"This bot is owned by {await bot.owner}")
+
+
+@listen()
+async def on_message_create(event):
+    # This event is called when a message is sent in a channel the bot can see
+    print(f"message received: {event.message.content}")
+
+
+bot.start(getenv("TOKEN"))
